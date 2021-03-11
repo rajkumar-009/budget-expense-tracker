f27ba9cf28b43a7da207cfe7791bf59f86a06e75
rajkumar-101 <rajmummies@gmail.com>
2021-03-11 13:23:13 -0700

    Updates

---
+++ precache-manifest.51e08c380c9fb5b3725a853b75973a54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe4efd59c15c19c9efbcaf7ba59f986",
+    "url": "/budget-expense-tracker/index.html"
+  },
+  {
+    "revision": "5742d17606b1f29cb29c",
+    "url": "/budget-expense-tracker/static/css/main.caf1db2f.chunk.css"
+  },
+  {
+    "revision": "0d4f83ac9df8803f639a",
+    "url": "/budget-expense-tracker/static/js/2.62e07433.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget-expense-tracker/static/js/2.62e07433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5742d17606b1f29cb29c",
+    "url": "/budget-expense-tracker/static/js/main.dcda1a3f.chunk.js"
+  },
+  {
+    "revision": "bee317de86505942c914",
+    "url": "/budget-expense-tracker/static/js/runtime-main.980385ae.js"
+  }
+]);
